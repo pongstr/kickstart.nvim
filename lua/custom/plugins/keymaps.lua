34ddcc3a1bef00e468100fc543c2c7fcd87c7daf
Pongstr <twiz.tickler@gmail.com>
2024-03-21 00:04:02 +0200

    fixed: nagging lint errors

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -1,5 +1,5 @@
 -- Toggle relative line numbers
-vim.api.nvim_set_keymap('n', '<leader>ln', ':set relativenumber!<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ln', ':set relativenumber!<CR>', { noremap = true })
 
 -- Show Undo List with Telescope
 vim.keymap.set('n', '<leader>u', ':Telescope undo<CR>', { desc = '[U]ndo List' })
