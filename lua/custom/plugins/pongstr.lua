8be357978fbfa71d965c182cbdeedfa7679c3443
Pongstr <twiz.tickler@gmail.com>
2024-03-25 23:10:10 +0200

    added a cheatsheet

--- lua/custom/plugins/pongstr.lua
+++ lua/custom/plugins/pongstr.lua
@@ -138,4 +138,37 @@ return {
       -- 'rcarriga/nvim-notify',
     },
   },
+
+  {
+    'ghillb/cybu.nvim',
+    branch = 'main', -- timely updates
+    -- branch = "v1.x", -- won't receive breaking changes
+    requires = { 'nvim-tree/nvim-web-devicons', 'nvim-lua/plenary.nvim' }, -- optional for icon support
+    config = function()
+      local ok, cybu = pcall(require, 'cybu')
+      if not ok then
+        return
+      end
+      cybu.setup {
+        position = {
+          relative_to = 'win', -- win, editor, cursor
+          anchor = 'topcenter', -- topleft, topcenter, topright,
+          -- centerleft, center, centerright,
+          -- bottomleft, bottomcenter, bottomright
+          vertical_offset = 10, -- vertical offset from anchor in lines
+          horizontal_offset = 0, -- vertical offset from anchor in columns
+          max_win_height = 5, -- height of cybu window in lines
+          max_win_width = 0.5, -- integer for absolute in columns
+          -- float for relative to win/editor width
+        },
+      }
+
+      vim.keymap.set('n', '<tab>', '<plug>(CybuLastusedNext)')
+      --
+      --   vim.keymap.set('n', 'T', '<Plug>(CybuPrev)')
+      --   vim.keymap.set('n', 'J', '<Plug>(CybuNext)')
+      --   vim.keymap.set({ 'n', 'v' }, '<c-s-tab>', '<plug>(CybuLastusedPrev)')
+      --   vim.keymap.set({ 'n', 'v' }, '<c-tab>', '<plug>(CybuLastusedNext)')
+    end,
+  },
 }
