4d307cca21204d6a010f6b1a0595b0a3661ba71b
Pongstr <twiz.tickler@gmail.com>
2024-03-29 15:45:54 +0200

    re-added

--- init.lua
+++ init.lua
@@ -230,6 +230,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-fugitive',
   'github/copilot.vim',
 
   -- NOTE: Plugins can also be added by using a table,
@@ -771,12 +772,25 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      require('tokyonight').setup {
+        -- Enable the "night" style
+        --  This is the default style, but you can change it to "storm", "day", or "night"
+        style = 'night',
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = false },
+          keywords = { italic = false },
+          sidebars = 'transparent', -- style for sidebars, see below
+          floats = 'transparent',
+        },
+      }
+      -- -- Load the colorscheme here.
+      -- -- Like many other themes, this one has different styles, and you could load
+      -- -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
+      --
+      -- -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
