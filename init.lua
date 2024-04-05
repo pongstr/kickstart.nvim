acdfd23871a1de38563f7d18f18ff0ad01be016c
Pongstr <twiz.tickler@gmail.com>
2024-04-05 22:10:15 +0300

    fix that shit dawg....

--- init.lua
+++ init.lua
@@ -396,6 +396,7 @@ require('lazy').setup({
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'undo')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
