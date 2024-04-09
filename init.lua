7a078ce0fb4aea3a956ac8fd885eeeabf823b251
Pongstr <twiz.tickler@gmail.com>
2024-04-09 23:18:51 +0300

    enable nerdfonts

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -231,7 +231,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'tpope/vim-fugitive',
-  'github/copilot.vim',
+  -- 'github/copilot.vim',
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -689,7 +689,7 @@ require('lazy').setup({
         json = { { 'prettierd', 'prettier', 'json' } },
         markdown = { { 'markdownlint' } },
         css = { { 'prettierd', 'prettier' } },
-        html = { { 'pretierd', 'prettier', 'html' } },
+        html = { { 'pretierd', 'prettier' } },
       },
     },
   },
